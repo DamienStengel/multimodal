1b3b2b3b2a2c06fb06917b8be68df2536e3c5a7a
Saad Beidouri <saad.beidouri@etu.unice.fr>
2024-12-11 11:50:54 +0100

    stop/start move

--- Assets/Resources/Scripts/ButtonsHandler.cs
+++ Assets/Resources/Scripts/ButtonsHandler.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,12 +7,20 @@ public class ButtonsHandler : MonoBehaviour
     private Button move;
     private Button teleport;
     private Button place_object;
+    
+    private VRTranslate vrTranslateScript;
 
     private void Start()
     {
         move = GameObject.Find("Move").GetComponent<Button>();
         teleport = GameObject.Find("Teleport").GetComponent<Button>();
         place_object = GameObject.Find("Place object").GetComponent<Button>();
+        
+        vrTranslateScript = GameObject.Find("Controller").GetComponent<VRTranslate>();
+        vrTranslateScript.enabled = false;
+        
+        
+        TextMeshProUGUI moveButtonText = move.GetComponentInChildren<TextMeshProUGUI>();
 
         move.onClick.AddListener(HandleMove);
         teleport.onClick.AddListener(HandleTeleport);
@@ -22,6 +31,17 @@ public class ButtonsHandler : MonoBehaviour
     private void HandleMove()
     {
         Debug.Log("Move button clicked");
+        
+        // Toggle the script on/off
+        vrTranslateScript.enabled = !vrTranslateScript.enabled;
+        
+        // Optional: Update button text to show state
+        TextMeshProUGUI moveButtonText = move.GetComponentInChildren<TextMeshProUGUI>();
+        if (moveButtonText != null)
+        {
+            moveButtonText.text = vrTranslateScript.enabled ? "Stop Move" : "Move";
+            moveButtonText.color = vrTranslateScript.enabled ? Color.red : Color.black;
+        }
     
     }
 
